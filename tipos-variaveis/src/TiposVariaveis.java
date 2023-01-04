7a4f65ccc32b5ab35fd0cc826f1dd20b51afce91
Julio Castro <juliordg@gmail.com>
2023-01-03 23:00:14 -0300

    atualizacao-1

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,7 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        double salario = 2500.45;
+        
     }
 }
